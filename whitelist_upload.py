9e09261cd5b2df29e9da5ec1dbb2a2c0dc56a6d9
Allen.C <allen@Allen.C>
2019-01-11 13:56:42 +0800

    完成了功能实现

--- whitelist_upload.py
+++ whitelist_upload.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 #coding=utf-8
+from __future__ import print_function
 
+import os
 import sys
+import time
 import getopt
 import tempfile
 import paramiko
 
+
+ACTIONS = ["add", "delete", "query"]
 
 class RHost(object):
     def __init__(self, host="127.0.0.1", port=22, user="root", passwd="", key_file="", **kwargs):
@@ -13,12 +18,13 @@ class RHost(object):
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.trans = paramiko.Transport((host,port))
+        pk = paramiko.RSAKey.from_private_key(open(key_file))
         if not key_file:
             self.conn= self.ssh.connect(hostname=host, username=user, port=port, password=passwd)
             self.trans.connect(username=user, password=passwd)
         else:
             self.conn= self.ssh.connect(hostname=host, username=user, port=port, password=passwd, key_filename=key_file)
-            self.trans.connect(username=user, password=passwd, pkey=key_file)
+            self.trans.connect(username=user, password=passwd, pkey=pk)
         self.sftp = paramiko.SFTPClient.from_transport(self.trans)
 
     def disconnect(self):
@@ -64,10 +70,92 @@ class RHost(object):
 
 
 def choose_action_type():
-    pass
+    choose_num = None
+    print("{0} {1} {0}".format("-"*16, "选择操作类型"))
+    for index, a in enumerate(ACTIONS):
+        print("{0:>10s} : {1}".format("[%d]" % index, a))
+    while choose_num is None:
+        choose_num = input("输入选项编号[2]:")
+        if choose_num.strip() == "":
+            choose_num = len(ACTIONS) - 1
+        try:
+            choose_num = int(choose_num)
+            if choose_num in range(len(ACTIONS)):
+                break
+        except:
+            pass
+        print("选择编号不在范围内，请重新选择")
+        choose_num = None
+        continue
+    print("{0} {1} {0}".format("-"*16, "操作类型选择结束"))
+    return ACTIONS[choose_num]
 
 def choose_ips():
-    pass
+    ips = []
+    is_right = False
+    print("{0} {1} {0}".format("-"*16, "输入IP值"))
+    while not is_right:
+        ips = input("输入被添加的ip值，多IP之间以空格隔开，输入回车结束输出：")
+        ips = ips.split()
+        if not len(ips) or not check_ips(ips):
+            print("输入IP中存在不合法的值，请重新输入")
+            continue
+        is_right = True
+    print("{0} {1} {0}".format("-"*16, "输入结束"))
+    return ips
+
+def reload_file(checkfile, action_type, ips):
+    infos = []
+    exists_ips = []
+    tmp_writefile = tempfile.mktemp()
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    is_deny = False
+    with open(checkfile) as fobj, open(tmp_writefile, "w") as tfobj:
+        line = fobj.readline()
+        while line and not is_deny:
+            if not line.strip():
+                line = fobj.readline()
+                continue
+            if line.split("#")[0].strip().split() == ["deny", "all;"]:
+                is_deny == True
+                line = fobj.readline()
+                continue
+            line_ip = line.split("#")[0].strip().split()[-1]
+            line_ip = line_ip.replace(";", "")
+            for ip in ips:
+                is_once = True
+                if is_once and not (ip == line_ip and action_type == "delete") and not action_type == "query":
+                    tfobj.write(line)
+                    is_once = False
+                if ip == line_ip and line_ip not in exists_ips and action_type != "delete":
+                    infos.append("目标IP {} 已存在文件中".format(ip))
+                if ip ==line_ip and line_ip not in exists_ips:
+                    exists_ips.add(ip)
+                if ip == line_ip:
+                    break
+            line = fobj.readline()
+        for ip in ips:
+            if ip not in exists_ips:
+                if action_type == "query":
+                    infos.append("目标IP {} 不存在文件中".format(ip))
+                elif action_type == "add":
+                    tfobj.write("allow {0}; #{1}\n".format(ip, timestamp))
+        if is_deny:
+            tfobj.write("deny all;\n")
+        while line:
+            tfobj.write(line)
+            line = fobj.readline()
+    for ele in infos:
+        print(ele)
+    return True
+
+def showInfo(item, info):
+    if not info:
+        info = "完成【%s】" % item
+    else:
+        info = "{0} 开始【{1}】错误信息 {0}\n{2}\n{0} 结束【{1}】错误信息 {0}".format("-"*16, item, info)
+    print(info)
+    return True
 
 def single_reload(client, localf, remotef):
     upload_ret = client.upload_file(localf, remotef)
@@ -98,24 +186,29 @@ def check_ips(ip_list):
     for ip in ip_list:
         if len(ip.split(".")) != 4:
             return False
-        for ele in ip.split("."):
-            if ele >= 255 or ele <= 0:
-                return False
+        try:
+            for ele in ip.split("."):
+                ele = int(ele)
+                if ele > 255 or ele < 0:
+                    return False
+        except:
+            return False
     return True
 
 def main():
     #{"host":"", "user":"", "port":22, "passwd":"", "key_file":"", "remotef":""}
     clients = [
+    {"host":"47.52.47.71", "key_file":"/home/allen/.ssh/ali1", "remotef":"/usr/local/nginx-1.14/conf/allows.ip", "user":"snowchan"},
     ]
     tmpf = tempfile.mktemp()
-    cfgfile = ""
+    cfgfile = "/usr/local/webserver/nginx/conf/grant_ips/front.zone"
 
     #获取命令行传入的参数
     vars_dict = check_options()
 
     #获取操作类型
     action_type = vars_dict.get("action_type", "")
-    if not action_type or action_type not in ["add", "delete", "query"]:
+    if not action_type or action_type not in ACTIONS:
         action_type = choose_action_type()
 
     #获取操作目标IP
@@ -127,24 +220,33 @@ def main():
     check_client = clients[0]
     check_c = RHost(**check_client)
     rfile = check_client.get("remotef", cfgfile)
-    checkfile = check_c.download_file(localf=tmpf, remotef=rfile)
+    download_ret = check_c.download_file(localf=tmpf, remotef=rfile)
     check_c.disconnect()
+    if not download_ret[0]:
+        showInfo("获取文件模板", download_ret[1])
+        return False
+    showInfo("获取文件模板", "")
 
     #刷新模板文件
-    reload_file(checkfile, action_type, ips)
+    reload_ret = reload_file(tmpf, action_type, ips)
+    showInfo("刷新文件模板", "")
 
     #将模板文件同步到所有客户端
+    if action_type == "query":
+        return True
     exe_ret = []
     for client in clients:
         c = RHost(**client)
-        rfile = c.get("remotef", cfgfile)
+        rfile = client.get("remotef", cfgfile)
         tmp_ret = single_reload(c, localf=tmpf, remotef=rfile)
         exe_ret.append({c.hostname: tmp_ret})
+        if not tmp_ret[0]:
+            showInfo("同步文件模板到%s" % c.hostname,tmp_ret[1])
+        showInfo("同步文件模板到%s" % c.hostname, "")
         c.disconnect()
 
     return True
 
 
-
 if __name__ == "__main__":
-    main()
+    sys.exit(0) if main() else sys.exit(1)
